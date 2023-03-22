2f85d2246678d501848f6813036bcb4ec626553c
Quim Crous <joaquimcrous@gmail.com>
2023-03-22 12:25:17 +0100

    Added Frontend directory

---
+++ frontend/genshin-web-wiki/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onUpdated, watchEffect, reactive } from "vue";
+import Proba from "../components/proba.vue";
+const response = ref(null);
+
+const fetchData = async () => {
+  const data = await fetch("../../public/characters_data.json");
+  response.value = await data.json();
+  console.log(response.value);
+  console.log("patata");
+};
+
+fetchData();
+</script>
+
+<template>
+  <Proba v-for="character in response" :character="character" />
+</template>
