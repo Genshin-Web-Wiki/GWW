65b95a4759ea5d3a75860b7c38ef4c362987d9a1
Quim Crous <joaquimcrous@gmail.com>
2023-03-23 10:47:09 +0100

    Added routing and imports of jsons

---
+++ frontend/genshin-web-wiki/src/views/Character.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import Characters from "../../public/characters_info.json";
+import Premium from "../../public/characters_premium.json";
+import { useRoute } from "vue-router";
+
+const characterInfo = ref(null);
+const teamPremium = ref(null);
+const route = useRoute();
+const boolean = ref(false);
+onMounted(() => {
+  const characterName = route.params.name;
+
+  characterInfo.value = Characters.find(
+    (character) => character.name === characterName
+  );
+
+  teamPremium.value = Premium.find((character) =>
+    characterName.includes(character.name)
+  );
+
+  console.log(teamPremium.value);
+  boolean.value = true;
+});
+</script>
+
+<template>
+  <div>
+    <h1>This is an about page</h1>
+    <p v-if="boolean">{{ characterInfo.name }}</p>
+    <p v-if="boolean">{{ teamPremium.name }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ frontend/genshin-web-wiki/src/views/Home.vue
@@ -0,0 +1,56 @@
+<script setup>
+import {
+  ref,
+  onUpdated,
+  watchEffect,
+  reactive,
+  onMounted,
+  computed,
+} from "vue";
+import Characters from "../../public/characters_info_sorted.json";
+
+// const firstJson = ref(null);
+// const secondJson = ref(null);
+// const charArray = ref(null);
+
+// onMounted(async () => {
+//   const response1 = await fetch("../../characters_info.json");
+//   const response2 = await fetch("../../characters_premium.json");
+
+//   firstJson.value = await response1.json();
+//   secondJson.value = await response2.json();
+//   console.log("papapapap");
+//   console.log(firstJson.value);
+//   console.log(secondJson.value);
+
+//   const matching = [];
+//   firstJson.value.forEach((item) => {
+//     const otherItem = secondJson.value.find((other) =>
+//       item.name.includes(other.name)
+//     );
+
+//     if (otherItem) {
+//       matching.push({
+//         name: item.name,
+//         value: item,
+//         premium: otherItem,
+//       });
+//     }
+//   });
+//   console.log(matching);
+//   charArray.value = matching;
+// });
+</script>
+
+<template>
+  <div>
+    <h1>Characters</h1>
+    <ul>
+      <li v-for="character in Characters" :key="character.name">
+        <router-link :to="'/' + character.name">{{
+          character.name
+        }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
